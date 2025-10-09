18084100da73c058d2bbab99fb7cc69e1cd9155a
cheesyGarlicBread15 <160604792+cheesyGarlicBread15@users.noreply.github.com>
2025-10-10 06:31:35 +0800

    added auth, no roles and permissions yet

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', fn() => Inertia::render('Home'));
-Route::get('/map', fn() => Inertia::render('Map'));
-Route::get('/profile', fn() => Inertia::render('Profile'));
+Route::get('/', function () {
+    return Inertia::render('Auth/Login');
+});
+
+Route::get('/home', function () {
+    return Inertia::render('Home');
+})->middleware(['auth', 'verified'])->name('home');
+Route::get('/map', function () {
+    return Inertia::render('Map');
+})->middleware(['auth', 'verified'])->name('map');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
